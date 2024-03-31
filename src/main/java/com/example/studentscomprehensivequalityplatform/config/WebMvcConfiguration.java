8a758d5f5112b6f9f4083d5dbb661190d1eb82a6
xingyue <2636584407@qq.com>
2024-03-31 23:25:49 +0800

    SpringMVC消息转换器设置

--- src/main/java/com/example/studentscomprehensivequalityplatform/config/WebMvcConfiguration.java
+++ src/main/java/com/example/studentscomprehensivequalityplatform/config/WebMvcConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.studentscomprehensivequalityplatform.config;
 
+import com.example.studentscomprehensivequalityplatform.common.json.JacksonObjectMapper;
 import com.example.studentscomprehensivequalityplatform.interceptor.JwtTokenStuOrganizationInterceptor;
 import com.example.studentscomprehensivequalityplatform.interceptor.JwtTokenStudentInterceptor;
 import com.example.studentscomprehensivequalityplatform.interceptor.JwtTokenTeacherInterceptor;
@@ -7,6 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -17,6 +20,8 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @Configuration
 @Slf4j
@@ -49,4 +54,17 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
                 .excludePathPatterns("/teacher/common/login");
     }
 
+    /**
+     * 扩展spring MVC框架的消息转化器
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        //创建一个消息转换器对象
+        MappingJackson2HttpMessageConverter converter= new MappingJackson2HttpMessageConverter();
+        //为消息转换器设置一个对象转换器，将java转为json数据
+        converter.setObjectMapper(new JacksonObjectMapper());
+        //将自己的消息转换器加入容器中
+        converters.add(0,converter);
+    }
 }
